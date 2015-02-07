cce6de697b9c4c6214bdfb593c422dab1e03c83f
whn09 <wanghenan09@gmail.com>
2015-02-07 17:39:46 +0800

    imessage is the same with article

--- public/modules/imessages/config/imessages.client.config.js
+++ public/modules/imessages/config/imessages.client.config.js
@@ -4,7 +4,7 @@
 angular.module('imessages').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Imessages', 'imessages', 'dropdown', '/imessages(/create)?');
+		Menus.addMenuItem('topbar', 'IMessages', 'imessages', 'dropdown', '/imessages(/create)?');
 		Menus.addSubMenuItem('topbar', 'imessages', 'List IMessages', 'imessages');
 		Menus.addSubMenuItem('topbar', 'imessages', 'New IMessage', 'imessages/create');
 	}
